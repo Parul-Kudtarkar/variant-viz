54660a9ad8cb3f5c01477d1b43b50f0c1c859ab0
smorabit <sam.morabito@yahoo.com>
2018-06-07 12:43:02 -0700

    first commit

---
+++ scripts/test_varsearch.py
@@ -0,0 +1,129 @@
+import dash
+import dash_core_components as dcc 
+import dash_html_components as html
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+import numpy as np
+import json
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from variant_viz import VariantViz
+from dash.dependencies import Input, Output
+import urllib.request
+import time
+
+#initialize dash app object:
+app = dash.Dash()
+
+#initializae VariantViz object:
+vv = VariantViz()
+
+#initialize var_data
+#var_name = "rs11257655"
+var_name = "rs7903146"
+with urllib.request.urlopen(vv.rsid_url(var_name)) as url:
+	var_data = json.loads(url.read())
+all_annotations = [key for key in var_data.keys()]
+
+#initialize number of button clicks:
+rsid_button_clicks = 0
+
+app.layout = html.Div(children=[
+
+	dcc.Graph(id='test-interactivity',figure=vv.make_graph(var_data=var_data, var_name=var_name)),
+
+	html.Div(children=[
+		dcc.Input(
+			id='rsid-input',
+			value=var_name,
+			type="text"
+		),
+		html.Button('update', id='rsid-button'),
+		dcc.RadioItems(
+			id='expand-radio',
+			options = [
+				{'label': "more", 'value': "more"},
+				{'label': "less", 'value': "less"}
+			],
+			value = "more",
+		),
+		dcc.Dropdown(
+			id='annotation-dropdown',
+			#obtain options directly from var data:
+			options=[{'label': anno, 'value': anno} for anno in all_annotations],
+			value=[key for key in var_data.keys()],
+			multi=True
+		),
+		html.Div(children=[])
+
+	], style={'columnCount': 1})
+
+])
+
+
+@app.callback(
+	Output('test-interactivity', 'figure'),
+	[Input('annotation-dropdown', 'value'),
+	 Input('expand-radio', 'value'),
+	 Input('rsid-input', 'value'),
+	 Input('rsid-button', 'n_clicks')])
+def update_graph(annot_value, expand_value, new_rsid, num_clicks):
+	print("updating graph")
+	#declare globals
+	global var_name
+	global var_data
+	global all_annotations
+	global rsid_button_clicks
+	print("num_clicks:", num_clicks)
+	print("rsid_button_clicks", rsid_button_clicks)
+	#try to update to new rsid
+	if num_clicks is not None and num_clicks == rsid_button_clicks + 1:
+		rsid_button_clicks += 1
+		if new_rsid != var_name:
+			with urllib.request.urlopen(vv.rsid_url(new_rsid)) as url:
+				u = url.read()
+				if u != bytes():
+					var_data = json.loads(u)
+					var_name = new_rsid
+					#all_annotations = [key for key in var_data.keys()]
+					return vv.make_graph(var_data=var_data, var_name=var_name)
+				else:
+					print("not a valid rsid:", new_rsid)
+
+	#make new dict using only checked values:
+	new_data = {key:val for key, val in var_data.items() if key in annot_value}
+
+	#expand or not?
+	if expand_value == "more":
+		expanded = True
+	else:
+		expanded = False
+
+	return vv.make_graph(var_data=var_data, var_name=var_name, subset_data=new_data, expanded=expanded)
+
+#update menu choices when var_name changes:
+@app.callback(
+	Output('annotation-dropdown', 'options'),
+	[Input('rsid-input', 'value'),
+	 Input('rsid-button', 'n_clicks')])
+def update_dropdown(new_rsid, num_clicks):
+
+	global rsid_button_clicks
+	global all_annotations
+
+	if num_clicks is not None and num_clicks == rsid_button_clicks + 1:
+		print("updating dropdown menu")
+		print("annotations:", all_annotations)
+		all_annotations = [key for key in var_data.keys()]
+
+	return [{'label': anno, 'value': anno} for anno in all_annotations]
+
+
+
+if __name__ == '__main__':
+	app.run_server(debug=True)
+
+
+
+
---
+++ scripts/variant_viz.py
@@ -0,0 +1,334 @@
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+import json
+import numpy as np
+
+class VariantViz:
+
+	dummy_name = "rs11257655"
+	dummy_data = {
+		"chromatin_state": [
+			{
+				"state": "enhA1",
+				"biosample_term_name": "skeletal muscle"
+			},
+			{
+				"state": "promoter",
+				"biosample_term_name": "islet"
+			}
+		],
+		"footprints": [
+			{
+				"footprint": "HNF4",
+				"biosample_term_name": "islet"
+			},
+			{
+				"footprint": "PPARA",
+				"biosample_term_name": "islet",
+			},
+			{
+				"footprint": "TCF3",
+				"biosample_term_name": "hippocampus"
+			},
+			{
+				"footprint": "STAT",
+				"biosample_term_name": "skeletal muscle"
+			}
+			
+		],
+
+		"ChIP": [
+			{
+				"target": "FOXA2",
+				"biosample_term_name": "islet",
+			},
+			{
+				"target": "NKX2.2",
+				"biosample_term_name": "skeletal muscle"
+			},
+			{
+				"target": "CTCF",
+				"biosample_term_name": "islet"
+			},
+		]
+		
+	}
+
+	def generate_positions(self, var_data=dummy_data, var_name=dummy_name, vert_space=4, box_width=10, \
+						   box_height=4, text_y0=100, plot_width=100, offset=20, expanded=True):
+		
+		'''
+		print("var name:", var_name)
+		for anno in var_data.keys():
+			print(anno, len(var_data[anno]))
+		'''
+		#get number of annotations
+		num_annotations = len(var_data.keys())
+		num_sides = int(num_annotations/2)
+		
+		positions = {
+			var_name: {
+				"text-coords": [plot_width/2, text_y0],
+				"shape-coords": [
+					[(plot_width/2)-box_width/2, text_y0-box_height/2],
+					[(plot_width/2)+box_width/2, text_y0+box_height/2]
+				],
+				"annotations": {key: {"items": val} for key, val in var_data.items()}
+			}
+		}
+		
+		for i, anno in enumerate(positions[var_name]["annotations"].keys()):
+			#get x offset:
+			if num_annotations % 2 == 1 and i == int(num_annotations/2)+1:
+				x_offset = positions[var_name]["text-coords"][0] + offset
+			elif i+1 <= num_sides: 
+				x_offset = positions[var_name]["text-coords"][0] - offset*(i+1)
+			elif i+1 > num_sides and num_annotations % 2 == 1:
+				x_offset = positions[var_name]["text-coords"][0] + offset*(i-int(num_annotations/2))
+			else:
+				x_offset = positions[var_name]["text-coords"][0] + offset*((i+1)-int(num_annotations/2))
+				
+			#set text coordinates for this annotation
+			positions[var_name]["annotations"][anno]["text-coords"] = [
+				x_offset, 
+				text_y0-(2*vert_space)
+			]
+			
+			#set shape coordinates for this annotation
+			positions[var_name]["annotations"][anno]["shape-coords"] = [
+				[
+					positions[var_name]["annotations"][anno]["text-coords"][0]-box_width/2,
+					positions[var_name]["annotations"][anno]["text-coords"][1]-box_height/2
+				],
+				[
+					positions[var_name]["annotations"][anno]["text-coords"][0]+box_width/2,
+					positions[var_name]["annotations"][anno]["text-coords"][1]+box_height/2
+				]
+			]
+			
+			if expanded == True:
+				for j, item in enumerate(positions[var_name]["annotations"][anno]["items"]):
+					if j == 0:
+						item["text-coords"] = [
+							positions[var_name]["annotations"][anno]["text-coords"][0],
+							positions[var_name]["annotations"][anno]["text-coords"][1] - box_height
+						]
+					else:
+						item["text-coords"] = [
+							positions[var_name]["annotations"][anno]["items"][j-1]["text-coords"][0],
+							positions[var_name]["annotations"][anno]["items"][j-1]["text-coords"][1] - box_height
+						]
+					item["shape-coords"] = [
+						[item["text-coords"][0]-box_width/2, item["text-coords"][1]-box_height/2],
+						[item["text-coords"][0]+box_width/2, item["text-coords"][1]+box_height/2]
+					]
+					#print("item:", item["biosample_term_name"], item["text-coords"])
+					#print("anno", anno, positions[var_name]["annotations"][anno]["text-coords"])
+
+		return positions
+
+	def generate_shapes(self, var_data, var_name, colors="", expanded=True):
+
+		shape_coords = [var_data[var_name]["shape-coords"]]
+		shapes = []
+
+		for anno in var_data[var_name]["annotations"].keys():
+			shape_coords.append(var_data[var_name]["annotations"][anno]["shape-coords"])
+			if expanded == True:
+				for item in var_data[var_name]["annotations"][anno]["items"]:
+					shape_coords.append(item["shape-coords"])
+
+		if colors == "":
+			colors = ['#888' for i in range(len(shape_coords))]
+		
+		for i, coords in enumerate(shape_coords):
+			shapes.append({
+				'type': 'rect',
+				'xref': 'x',
+				'yref': 'y',
+				'x0': coords[0][0],
+				'y0': coords[0][1],
+				'x1': coords[1][0],
+				'y1': coords[1][1],
+				'line': {
+					'color': '#888',
+					'width': 2,
+				},
+				'fillcolor': 'rgba(55, 128, 191, 0.6)',
+				'opacity': 0.5
+			})
+		return shapes
+
+	def invisible_points(self, positions, var_name):
+		
+		min_coords = [i for i in positions[var_name]["text-coords"]]
+		max_coords = [i for i in positions[var_name]["text-coords"]]
+		
+		for anno, info in positions[var_name]["annotations"].items():
+			if info["text-coords"][0] > max_coords[0]:
+				max_coords[0] = info["text-coords"][0]
+			elif info["text-coords"][0] < min_coords[0]:
+				min_coords[0] = info["text-coords"][0]
+			if info["text-coords"][1] > max_coords[1]:
+				max_coords[1] = info["text-coords"][1]
+			elif info["text-coords"][1] < min_coords[1]:
+				min_coords[1] = info["text-coords"][1]
+			
+			for item in positions[var_name]["annotations"][anno]["items"]:
+				if item["text-coords"][0] > max_coords[0]:
+					max_coords[0] = item["text-coords"][0]
+				elif item["text-coords"][0] < min_coords[0]:
+					min_coords[0] = item["text-coords"][0]
+				if item["text-coords"][1] > max_coords[1]:
+					max_coords[1] = item["text-coords"][1]
+				elif item["text-coords"][1] < min_coords[1]:
+					min_coords[1] = item["text-coords"][1]
+
+		#extend invisible x points to avoid text cutoffs
+		min_coords[0] -= 25
+		max_coords[0] += 25
+	
+		return min_coords, max_coords
+
+	#finds the longest label name
+	def max_text_len(self, var_data):
+		
+		max_text_len = 0
+		
+		for anno, items in var_data.items():
+			if len(anno) > max_text_len:
+				max_text_len = len(anno)
+			for item in items:
+				text = item["biosample_term_name"] + ": "
+				if "state" in item.keys():
+					text += "state"
+				elif "target" in item.keys():
+					text += "target"
+				elif "footprint" in item.keys():
+					text += "footprint"
+				if len(text) > max_text_len:
+					max_text_len = len(text)
+		
+		return max_text_len
+
+	def rsid_url(self, rsid):
+		return "https://www.t2depigenome.org/peak_metadata/region=%s&genome=GRCh37/peak_metadata.tsv" % rsid
+
+	def make_graph(self, var_data=dummy_data, var_name=dummy_name, subset_data="", vert_space=4, box_width=25, \
+				box_height=4, text_y0=100, plot_width=1000, plot_height=500, offset=20, expanded=True):
+
+		#get max number of items in one annotation
+		max_items = max([len(var_data[anno]) for anno in var_data.keys()])
+		num_annotations = len(var_data.keys())
+	
+		#compute plot width and height:
+		plot_height = 350 * np.log(max_items)
+		plot_width = 1200 * np.log(num_annotations)
+		box_width = self.max_text_len(var_data)
+		offset = box_width + 5
+
+		all_positions = self.generate_positions(var_data, var_name, expanded=expanded, box_height=box_height, \
+								   box_width=box_width, offset=offset, text_y0=text_y0)
+		if subset_data == "":
+			positions = self.generate_positions(var_data, var_name, expanded=expanded, box_height=box_height, \
+								   box_width=box_width, offset=offset, text_y0=text_y0)
+		else:
+			positions = self.generate_positions(subset_data, var_name, expanded=expanded, box_height=box_height, \
+								   box_width=box_width, offset=offset, text_y0=text_y0)
+
+		#initialize node and edge traces:
+		node_trace = go.Scatter(
+			x = [],
+			y = [],
+			text = [],
+			mode='markers+text',
+			hoverinfo='none',
+			marker=dict(
+				color=[],
+				size=10,
+				line=dict(width=10),
+				opacity=0.0
+			)
+		)
+
+		edge_trace = go.Scatter(
+			x = [],
+			y = [],
+			line=dict(width=2,color='#888'),
+			hoverinfo='none',
+			mode='lines'		
+		)
+		
+		#populate node information with text positions:
+		node_trace['x'].append(positions[var_name]["text-coords"][0])
+		node_trace['y'].append(positions[var_name]["text-coords"][1])
+		node_trace['text'].append(var_name)
+		for anno in positions[var_name]["annotations"].keys():
+			node_trace['x'].append(positions[var_name]["annotations"][anno]["text-coords"][0])
+			node_trace['y'].append(positions[var_name]["annotations"][anno]["text-coords"][1])
+			node_trace['text'].append(anno)
+			if expanded == True:
+				for item in positions[var_name]["annotations"][anno]["items"]:
+					node_trace['x'].append(item["text-coords"][0])
+					node_trace['y'].append(item["text-coords"][1])
+					text = item["biosample_term_name"] + ": "
+					if "state" in item.keys():
+						text += item['state']
+					elif "footprint" in item.keys():
+						text += item['footprint']
+					elif "target" in item.keys():
+						text += item['target']
+					node_trace['text'].append(text)
+
+		#fill in edges between variant name and annotation names:
+		for anno in positions[var_name]["annotations"].keys():
+			edge_trace['x'] += [positions[var_name]["text-coords"][0], positions[var_name]["annotations"][anno]["text-coords"][0], None]
+			edge_trace['y'] += [positions[var_name]["shape-coords"][0][1], positions[var_name]["annotations"][anno]["shape-coords"][1][1], None]
+		
+		#add invisible points:
+		
+		min_coords, max_coords = self.invisible_points(all_positions, var_name)
+		node_trace['x'].append(min_coords[0])
+		node_trace['y'].append(min_coords[1])
+		node_trace['x'].append(max_coords[0])
+		node_trace['y'].append(max_coords[1])
+		
+		layout = go.Layout(
+			width=plot_width,
+			height=plot_height,
+			xaxis = dict(
+				showgrid=False, 
+				zeroline=False,
+				showline=False,
+				ticks='',
+				showticklabels=False
+			),
+			yaxis = dict(
+				showgrid=False,
+				zeroline=False,
+				showline=False,
+				ticks='',
+				showticklabels=False
+			),
+			showlegend=False,
+		)
+
+		layout['shapes'] = self.generate_shapes(positions, var_name, expanded=expanded)
+
+		data = [node_trace, edge_trace]
+		fig = go.Figure(data=data, layout=layout)
+		return fig
+		
+
+
+
+
+
+
+
+
+
+
+
